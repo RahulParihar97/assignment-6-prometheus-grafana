b12ca321154ebac37a4da3181589023c5e9fe0ad
Rahul Parihar <121428206+RahulParihar97@users.noreply.github.com>
2026-06-07 20:25:14 +0530

    Create monitoring-config.groovy

---
+++ monitoring-config.groovy
@@ -0,0 +1,15 @@
+return [
+
+    SLACK_CHANNEL_NAME  : "build-status",
+
+    ENVIRONMENT         : "prod",
+
+    CODE_BASE_PATH      : "ansible",
+
+    PLAYBOOK_NAME       : "install-prometheus.yml",
+
+    ACTION_MESSAGE      : "Prometheus deployment completed successfully",
+
+    KEEP_APPROVAL_STAGE : true
+
+]
