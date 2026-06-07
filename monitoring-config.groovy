e6c148ea706cfba38379ebcc02cac68aa3f4942b
Rahul Parihar <rahulparihar3829@gmail.com>
2026-06-07 16:08:19 +0000

    Added real ansible playbooks

--- monitoring-config.groovy
+++ monitoring-config.groovy
@@ -1,12 +1,14 @@
 return [
 
-    SLACK_CHANNEL_NAME  : "build-status",
+    SLACK_CHANNEL_NAME  : "jenkins-alerts",
 
     ENVIRONMENT         : "prod",
 
     CODE_BASE_PATH      : "ansible",
 
     PLAYBOOK_NAME       : "install-prometheus.yml",
+    
+    PLAYBOOK_NAME       : "install-grafana.yml",
 
     ACTION_MESSAGE      : "Prometheus deployment completed successfully",
 
